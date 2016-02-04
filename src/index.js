29036bd48c3958e7b93265b5c16cbfdce86de466
Barrett Alexander <barrett@clockwork.net>
2016-02-04 10:59:26 -0600

    Added statement pool

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ module.exports = {
     Dispatcher: require('./dispatcher'),
     Exchange: require('./exchange'),
     Request: require('./request'),
+    StatementPool: require('./statement-pool'),
     Storage: require('./storage'),
     Trickle: require('./trickle'),
     Typist: require('./typist'),
